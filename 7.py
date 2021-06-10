e416897224243ae7b933b3905d44d97a0b05780e
ACER <manjeshsilwal@gmail.com>
2021-06-10 19:20:37 +0545

    commit

---
+++ 7.py
@@ -0,0 +1,4 @@
+'''You live 4 miles from university. The bus drives at 25mph but spends 2 minutes at each of the 10 stops on the way. How long will the bus journey take?
+Alternatively, you could run to university.
+You jog the first mile at 7mph; then run the next two at15mph;before jogging the last at 7mph again. Will this be quicker or slower than the bus?
+'''
